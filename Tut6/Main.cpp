e9802ade2a5e9f48bce787a4804ffffc35ec01ef
Salihan04 <salihan04@gmail.com>
2014-12-22 14:42:06 +0800

    Generate random colours for cube each frame

--- Tut6/Main.cpp
+++ Tut6/Main.cpp
@@ -4,6 +4,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 #include "LoaderShader.h"
 
 using namespace std;
@@ -13,6 +15,7 @@ void renderScene();
 void createCubeVertexBuffer();
 void createCubeColorBuffer();
 void initialiseGlutCallback();
+GLfloat generateRandNum();
 
 GLuint cubeVertexBuffer;
 GLuint cubeColorBuffer;
@@ -24,6 +27,7 @@ glm::mat4 model;
 glm::mat4 MVP;
 
 int main(int argc, char **argv) {
+	srand(time(0));
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
@@ -113,7 +117,7 @@ void createCubeVertexBuffer() {
 	//3 consecutive vertices give a triangle.
 	//a cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, 
 	//and 12*3 vertices
-	static const GLfloat vertices[] = {
+	GLfloat vertices[] = {
 		-1.0f, -1.0f, -1.0f,		// triangle 1 : begin
 		-1.0f, -1.0f, 1.0f,
 		-1.0f, 1.0f, 1.0f,			// triangle 1 : end
@@ -158,44 +162,16 @@ void createCubeVertexBuffer() {
 }
 
 void createCubeColorBuffer() {
-	static const GLfloat colors[] = {
-		0.583f, 0.771f, 0.014f, 1.0f,
-		0.609f, 0.115f, 0.436f, 1.0f,
-		0.327f, 0.483f, 0.844f, 1.0f,
-		0.822f, 0.569f, 0.201f, 1.0f,
-		0.435f, 0.602f, 0.223f, 1.0f,
-		0.310f, 0.747f, 0.185f, 1.0f,
-		0.597f, 0.770f, 0.761f, 1.0f,
-		0.559f, 0.436f, 0.730f, 1.0f,
-		0.359f, 0.583f, 0.152f, 1.0f,
-		0.483f, 0.596f, 0.789f, 1.0f,
-		0.559f, 0.861f, 0.639f, 1.0f,
-		0.195f, 0.548f, 0.859f, 1.0f,
-		0.014f, 0.184f, 0.576f, 1.0f,
-		0.771f, 0.328f, 0.970f, 1.0f,
-		0.406f, 0.615f, 0.116f, 1.0f,
-		0.676f, 0.977f, 0.133f, 1.0f,
-		0.971f, 0.572f, 0.833f, 1.0f,
-		0.140f, 0.616f, 0.489f, 1.0f,
-		0.997f, 0.513f, 0.064f, 1.0f,
-		0.945f, 0.719f, 0.592f, 1.0f,
-		0.543f, 0.021f, 0.978f, 1.0f,
-		0.279f, 0.317f, 0.505f, 1.0f,
-		0.167f, 0.620f, 0.077f, 1.0f,
-		0.347f, 0.857f, 0.137f, 1.0f,
-		0.055f, 0.953f, 0.042f, 1.0f,
-		0.714f, 0.505f, 0.345f, 1.0f,
-		0.783f, 0.290f, 0.734f, 1.0f,
-		0.722f, 0.645f, 0.174f, 1.0f,
-		0.302f, 0.455f, 0.848f, 1.0f,
-		0.225f, 0.587f, 0.040f, 1.0f,
-		0.517f, 0.713f, 0.338f, 1.0f,
-		0.053f, 0.959f, 0.120f, 1.0f,
-		0.393f, 0.621f, 0.362f, 1.0f,
-		0.673f, 0.211f, 0.457f, 1.0f,
-		0.820f, 0.883f, 0.371f, 1.0f,
-		0.982f, 0.099f, 0.879f, 1.0f
-	};
+	GLfloat colors[144];
+	for (int i = 0; i < 144; i++) {
+		colors[i] = generateRandNum();
+	}
+
+	int j = 3;
+	while (j < 12 * 3 * 4) {
+		colors[j] = 1.0f;
+		j += 4;
+	}
 
 	glGenBuffers(1, &cubeColorBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, cubeColorBuffer);
@@ -205,4 +181,8 @@ void createCubeColorBuffer() {
 void initialiseGlutCallback() {
 
 	glutDisplayFunc(renderScene);
+}
+
+GLfloat generateRandNum() {
+	return ((GLfloat)rand()) / RAND_MAX;
 }
